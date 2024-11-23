e87951f4b13e3e252ae1e96543d41ff6b73b55cc
Davut <2005davut55@gmail.com>
2024-11-23 15:43:27 +0200

    Twitter Icon and Login Form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
+      {/* Header with Twitter Logo */}
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <FontAwesomeIcon icon={faTwitter} className="twitter-logo" />
       </header>
+
+      {/* Login Form */}
+      <div className="login-form-container">
+        <h2>Login</h2>
+        <form>
+          <div className="form-group">
+            <label htmlFor="name">Name</label>
+            <input type="text" id="name" placeholder="Enter your name" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" placeholder="Enter your email" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" placeholder="Enter your password" />
+          </div>
+          <button type="submit">Login</button>
+        </form>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
