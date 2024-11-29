6ba3b6f25ddbe5bb81c364524f212d722b39ff8e
Begenchhh <begenchrejebov@gmail.com>
2024-11-29 23:01:43 +0500

    just curly braces

--- src/App.js
+++ src/App.js
@@ -1,35 +1,20 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import Sidebar from "./Sidebar";
 import './App.css';
 
 const App = () => {
   return (
-    <div className="App">
-      {/* Header with Twitter Logo */}
-      <header className="App-header">
-        <FontAwesomeIcon icon={faTwitter} className="twitter-logo" />
-      </header>
+    // BEM
+    <div className="app">
+      <h1>good</h1>
+      
+     {/* Sidebar */}
+       <Sidebar/>
+      {/* Feed */}
 
-      {/* Login Form */}
-      <div className="login-form-container">
-        <h2>Login</h2>
-        <form>
-          <div className="form-group">
-            <label htmlFor="name">Name</label>
-            <input type="text" id="name" placeholder="Enter your name" />
-          </div>
-          <div className="form-group">
-            <label htmlFor="email">Email</label>
-            <input type="email" id="email" placeholder="Enter your email" />
-          </div>
-          <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input type="password" id="password" placeholder="Enter your password" />
-          </div>
-          <button type="submit">Login</button>
-        </form>
-      </div>
+      {/* Widgets */}
+
+
     </div>
   );
 };
---
+++ src/Sidebar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import '.Sidebar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+        {/* Twitter Icon */}
+        <FontAwesomeIcon icon={faTwitter} size="2x" color="black"/>
+        {/* Sidebar Option */}
+        {/* Sidebar Option */}
+        {/* Sidebar Option */}
+        {/* Sidebar Option */}
+        {/* Sidebar Option */}
+        {/* Sidebar Option */}
+        {/* Sidebar Option */}
+        
+        {/* Button - Gep */}
+      </div>
+  )
+}
+
+export default Sidebar
